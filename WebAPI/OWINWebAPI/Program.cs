37314ff2c52698a00c5d7102124ff1cee224b75f
Suresh Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-10-31 19:58:37 +0530

    OWIN webApi sample

---
+++ WebAPI/OWINWebAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace OWINWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x => // x is Topshelf.HostConfigurators.HostConfigurator
+            {
+                x.Service<OWINService>();
+                x.RunAsLocalSystem(); //Runs the service using the local system account.
+                x.StartAutomatically(); //Sets whether the service should start automatically on system start-up
+            });
+        }
+    }
+    public class OWINService : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            string baseAddress = "http://localhost:9000/";
+            WebApp.Start<Startup>(baseAddress);
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            return true;
+        }
+    }
+}
